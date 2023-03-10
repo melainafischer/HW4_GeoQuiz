a7959ccf9a90d830adab7e52ab4308f48ddb6a23
Melaina Fischer <melainafischer@gmail.com>
2023-03-09 22:24:36 -0600

    Sun Feb 19

--- app/src/main/java/com/fischer/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/fischer/msu/geoquiz/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.fischer.msu.geoquiz
 
-
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.util.Log
+import com.fischer.msu.geoquiz.QuizViewModel
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.fischer.msu.geoquiz.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityMainBinding
-   // private lateinit var trueButton: Button
-   // private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+    private val quizViewModel:QuizViewModel by viewModels()
 
-
-    private val questionBank = listOf(
+/*    private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
         Question(R.string.question_mideast, false),
@@ -26,58 +24,124 @@ private lateinit var binding: ActivityMainBinding
         Question(R.string.question_asia, true)
     )
     private var currentIndex = 0
+    private var counterCorrect = 0
+    private var counterIncorrect = 0
+    private var scorePercentage = 0
+    private var scoreString = ""*/
+
+    private var counterCorrect = 0
+    private var counterIncorrect = 0
+    //private var scorePercentage = 0
+    //private var scoreString = ""
+    //private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        Log.d(TAG, "onCreate (Bundle?) called")
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-      //  trueButton = findViewById(R.id.true_button)
-      //  falseButton = findViewById(R.id.false_button)
+        Log.d(TAG, "Got a QuizViewModel:$quizViewModel")
 
-        binding.trueButton.setOnClickListener{
-            val snackbar = Snackbar.make(
-                it,
-                "Correct",
-                Snackbar.LENGTH_LONG
-            )
-            snackbar.show()
+        binding.trueButton.setOnClickListener {
+            checkAnswer(true)
+            binding.trueButton.isEnabled = !(binding.trueButton.isEnabled)
+            binding.falseButton.isEnabled = !(binding.falseButton.isEnabled)
         }
 
-        binding.falseButton.setOnClickListener{
-            val snackbar = Snackbar.make(
-                it,
-                "Incorrect",
-                Snackbar.LENGTH_LONG
-            )
-            snackbar.setTextColor(Color.BLACK)
-            snackbar.setBackgroundTint(Color.RED)
-            snackbar.show()
+        binding.falseButton.setOnClickListener {
+            checkAnswer(false)
+            binding.trueButton.isEnabled = !(binding.trueButton.isEnabled)
+            binding.falseButton.isEnabled = !(binding.falseButton.isEnabled)
         }
 
-        binding.nextButton.setOnClickListener{
-            currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            binding.questionTextview.setText(questionTextResId)
-            //updateQuestion()
+        binding.nextButton.setOnClickListener {
+            //currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
+            updateQuestion()
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
         }
+        updateQuestion()
 
-        binding.previousButton.setOnClickListener{
+       /* binding.previousButton.setOnClickListener{
             currentIndex = (currentIndex - 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            binding.questionTextview.setText(questionTextResId)
-            //updateQuestion()
-        }
+            updateQuestion()
+            binding.trueButton.isEnabled = !(binding.trueButton.isEnabled)
+            binding.falseButton.isEnabled = !(binding.falseButton.isEnabled)
+        }*/
 
-        binding.questionTextview.setOnClickListener{
+/*        binding.questionTextview.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            binding.questionTextview.setText(questionTextResId)
-        }
-
+            updateQuestion()
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
+        }*/
     }
 
-/*    private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        binding.questionTextview.setText(questionTextResId)
+    private fun updateQuestion() {
+        //val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
+
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        //val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+
+        //val correctAnswer = questionBank[currentIndex + 1] as Boolean
+
+        if (userAnswer == correctAnswer) {
+            //R.string.correct_toast
+            // add counter variable here to count number of correct answers
+            counterCorrect++
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
+
+        } else {
+            //R.string.incorrect_toast
+            counterIncorrect++
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
+        }
+        //Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show()
+        //calculateScore()
+    }
+/*    private fun calculateScore() {
+        if (currentIndex == questionBank.size - 1){
+            //val scorePercentage = (counterCorrect.toDouble() / questionBank.size * 100)
+            // String.format("%.1f", number2digits).toDouble()
+            val scorePercentage = (counterCorrect.toDouble() / questionBank.size.toDouble() * 100)
+            val scoreString = String.format("%.1f", scorePercentage) + "%"
+            Toast.makeText(this, scoreString, Toast.LENGTH_LONG).show()
+            counterCorrect = 0
+        //displayToast(scoreString)
+        }
+
+            //•	Reset number correct to 0
+        }*/
+/*    private fun displayToast(text: String) {
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+        toast.show()
     }*/
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
 }
