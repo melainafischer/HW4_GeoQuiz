7daefe56fa31d8a1a36e00254d7f20f08979558a
Melaina Fischer <melainafischer@gmail.com>
2023-03-09 22:25:20 -0600

    Sun Feb 19

---
+++ app/src/main/java/com/fischer/msu/geoquiz/QuizViewModel.kt
@@ -0,0 +1,47 @@
+package com.fischer.msu.geoquiz
+
+import android.util.Log
+import android.widget.TextView.SavedState
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.fischer.msu.geoquiz.R
+
+private const val TAG = "QuizViewModel"
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+
+
+class QuizViewModel(private val savedStateHandle:SavedStateHandle):ViewModel() {
+
+/*    init{
+        Log.d(TAG, "QuizViewModel instance created")
+    }
+
+    override fun onCleared(){
+        super.onCleared()
+
+        Log.d(TAG,"QuizViewModel instance about to be destroyed")
+    }*/
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex:Int
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY)?:0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
+
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext(){
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+}
